54eeff8f28c5f0467dcc31cff44453cda14e5bd6
nguyensadbkh-maker <nguyensadbkh@gmail.com>
2026-02-24 15:31:20 +0700

    Add files via upload

--- quanly.py
+++ quanly.py
@@ -303,7 +303,7 @@ if sh:
             st.cache_data.clear()
             st.rerun()
 
-    DANH_SACH_NHA = { "Tòa A": ["A101"], "Tòa B": ["B101"], "Khác": [] }
+    DANH_SACH_NHA = { "MT60": [], "MT61": [], "OC1A": [], "OC1B": [], "OC2A": [], "OC2B": [], "OC3": [] }
 
     # ==============================================================================
     # 6. GIAO DIỆN CHÍNH (TABS)
